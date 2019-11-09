3dddc4df32bbfbd1fa440e770cf36bbf0fb7bd2d
Takaaki Shirai <lain.op.rai@gmail.com>
2019-11-09 13:45:21 +0900

    Show content of file

--- main.go
+++ main.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+)
+
+func main() {
+	flag.Parse()
+	args := flag.Args()
+	if len(args) == 0 {
+		return
+	}
+	fmt.Println(args[0])
+
+	buf, err := ioutil.ReadFile(args[0])
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(buf))
+	return
+}
