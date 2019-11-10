698de7569978e5cecd42c323f47816013c03c0cc
Takaaki Shirai <lain.op.rai@gmail.com>
2019-11-10 19:46:54 +0900

    use FindAllStringSubmatch

--- increment/increment.go
+++ increment/increment.go
@@ -6,10 +6,20 @@ import (
 	"strconv"
 )
 
-const regExpPattern = "([0-9]+).([0-9]+).([0-9]+)"
+//const regExpPattern = `([0-9]+).([0-9]+).([0-9]+)(.[0-9]+)?`
+const regExpPattern = `([0-9]+).([0-9]+).([0-9]+)`
 
 func Increment(version string) string {
 	rep := regexp.MustCompile(regExpPattern)
-	minor, _ :=strconv.Atoi(rep.ReplaceAllString(version, "$3"))
-	return rep.ReplaceAllString(version, fmt.Sprintf("$1.$2.%d",minor+1))
+	result := rep.FindAllStringSubmatch(version, -1)
+	if len(result) != 1 {
+		return ""
+	}
+	versions := result[0][1:]
+	if len(versions) < 3 {
+		return ""
+	}
+	minor, _ := strconv.Atoi(versions[2])
+	return fmt.Sprintf("%s.%s.%d", versions[0], versions[1], minor+1)
+	return ""
 }
